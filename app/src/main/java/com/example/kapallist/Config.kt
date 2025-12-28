c3ce1769b715bca9d7e741bbb5d9697d42820cb2
MarbLe Studio <ahmad.dzikrus.sodiqin@gmail.com>
2025-12-28 18:35:06 +0700

    Dokumen Status

--- app/src/main/java/com/example/kapallist/Config.kt
+++ app/src/main/java/com/example/kapallist/Config.kt
@@ -2,5 +2,5 @@ package com.example.kapallist
 
 object Config {
     // Change this to your IDCloudHost domain
-    const val BASE_URL = "https://aplikasi-kapal-production.up.railway.app/"
+    const val BASE_URL = "https://kapallist-production.up.railway.app/"
 }
