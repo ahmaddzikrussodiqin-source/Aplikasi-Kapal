80683bc7b9df38ae252df196a97de19e5a5ed846
MarbLe Studio <ahmad.dzikrus.sodiqin@gmail.com>
2026-01-09 09:26:11 +0700

    maintenance 1.02.00

--- backend/server.js
+++ backend/server.js
@@ -518,7 +518,7 @@ app.get('/', (req, res) => {
     res.json({
         success: true,
         message: 'KapalList Backend API is running!',
-        version: '1.02.13',
+        version: '1.02.14',
         updateLink: 'https://drive.google.com/drive/folders/1--rbjtMEQtxAlJEYjxonWl-yf5vX3frP?usp=drive_link',
         timestamp: new Date().toISOString()
     });
