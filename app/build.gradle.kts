c76c7961e75ee28588d218526ca19acc21e191eb
MarbLe Studio <ahmad.dzikrus.sodiqin@gmail.com>
2026-01-21 22:36:39 +0700

    maintenance 1.02.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 4
-        versionName = "1.02.14"
+        versionName = "1.02.15"
     }
 
     buildTypes {
--- backend/server.js
+++ backend/server.js
@@ -518,8 +518,8 @@ app.get('/', (req, res) => {
     res.json({
         success: true,
         message: 'KapalList Backend API is running!',
-        version: '1.02.14',
-        updateLink: 'https://drive.google.com/file/d/1bJCQCbcAUeRng1e8mesUCF8ILf1VExzc/view?usp=sharing',
+        version: '1.02.15',
+        updateLink: 'https://drive.google.com/file/d/1yINdnW57QjcXiVT46t9e6bTTtb0vDwHE/view?usp=sharing',
         timestamp: new Date().toISOString()
     });
 });
