19c611b89ba1a6e6676703b26de612902179a460
ahmad dzikrus sodiqin <ahmad.dzikrus.sodiqin@gmail.com>
2026-01-29 19:01:47 +0700

    serv

--- website/server.js
+++ website/server.js
@@ -7,11 +7,18 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
 // Serve static files from the React app build directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle React Router - send all non-API requests to React app
 app.get('*', (req, res) => {
+  console.log('Serving index.html');
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
